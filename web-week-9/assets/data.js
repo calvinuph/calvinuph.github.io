b83b8aa83a46fe218ec2f63da5476701ce14694f
calvinuph <124014675+calvinuph@users.noreply.github.com>
2023-03-07 09:30:52 +0700

    Add files via upload

---
+++ web-week-9/assets/data.js
@@ -0,0 +1,23 @@
+var restos = [
+{
+  nama: "Pizza Apaya",
+  alamat: "Kepa Listrik No.1, RT.3/RW.4, Duri Kepa, Jakarta 11510",
+  status: "Tutup",
+  tel: "0858-6480-8748",
+  foto: "pizza.jpg"
+},
+{
+  nama: "Kebuli Apa-ya",
+  alamat: "Blk. RGB, Jl. Boulevard Raya No.11",
+  status: "Segera Buka",
+  tel: "0812-2331-2308",
+  foto: "kebuli.jpg"
+},
+{
+  nama: "Kopi Apaya",
+  alamat: "Jl. Lele Raya, RT.05/RW.05, Bambu Apus",
+  status: "Buka",
+  tel: "0813-1167-9171",
+  foto: "kopi.jpg"
+}
+];
